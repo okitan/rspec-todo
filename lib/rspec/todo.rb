b4305efb7c3e871ce3a00695d381f0badec5eb51
okitan <okitakunio@gmail.com>
2013-06-29 09:27:44 +0900

    todo can accept opts for if and unless

--- lib/rspec/todo.rb
+++ lib/rspec/todo.rb
@@ -1,26 +1,44 @@
+require "tapp"
+
 module RSpec
   module Todo
-    def todo(reason = nil, &block)
-      if block_given?
-        begin
-          yield
-        rescue => e
-          if reason
-            pending(reason)
-          else
-            pending(e.inspect)
-          end
-        end
+    def rspec_todo_works_for(opts = {})
+      if opts[:if].nil? && opts[:unless].nil?
+        true
       else
-        pending(reason)
+        opts[:if] || (opts[:unless].nil? ? false : !opts[:unless])
       end
     end
 
-    def not_todo(reason = nil, *errors_expected, &block)
+    def todo(reason = nil, opts = {}, &block)
+      reason, opts = nil, reason if reason.is_a?(Hash)
+
+      if rspec_todo_works_for(opts)
+        if block_given?
+          begin
+            yield
+          rescue => e
+            if reason
+              pending(reason)
+            else
+              pending(e.inspect)
+            end
+          end
+        else
+          pending(reason)
+        end
+      else
+        yield
+      end
+    end
+
+    def not_todo(reason = nil, opts = {}, &block)
+      reason, opts = nil, reason if reason.is_a?(Hash)
+
       if block_given?
         begin
           yield
-        rescue RSpec::Expectations::ExpectationNotMetError, *errors_expected => e
+        rescue RSpec::Expectations::ExpectationNotMetError, *opts[:errors] => e
           # do nothing
         else
           if reason
--- spec/rspec/todo_spec.rb
+++ spec/rspec/todo_spec.rb
@@ -1,10 +1,59 @@
 require "spec_helper"
 
+shared_examples "pending" do |sub_description, opts = {}|
+  context(sub_description, opts) do
+    it "works" do
+      begin
+        expect { expectation }.to raise_exception(pending_exception)
+      ensure
+        # if not example comes to be pending and cannot determine green or red
+        example.metadata[:pending] = false
+      end
+    end
+  end
+end
+
+shared_examples "error" do |sub_description, opts = {}|
+  context(sub_description, opts) do
+    it "works" do
+      begin
+        expect { expectation }.to raise_exception {|exception|
+          exception.should_not be_a(pending_exception)
+        }
+      ensure
+        # if not example may come to be pending and cannot detect failre
+        example.metadata[:pending] = false
+      end
+    end
+  end
+end
+
 describe "rspec-todo" do
+  let(:pending_exception) { ::RSpec::Core::Pending::PendingDeclaredInExample }
+
+  context "#rspec_todo_works_for" do
+    [ [ true,  true,  true ],
+      [ true,  false, true ],
+      [ true,  nil,   true ],
+      [ false, true,  false ],
+      [ false, false, true ],
+      [ false, nil,   false ],
+      [ nil,   true,  false ],
+      [ nil,   false, true ],
+      [ nil,   nil,   true ],
+    ].each do |if_value, unless_value, result|
+      it "returns #{result} when if is #{if_value.inspect} and unless is #{unless_value.inspect}" do
+        rspec_todo_works_for(if: if_value, unless: unless_value).should == result
+      end
+    end
+  end
+
   context "todo" do
     context "with description" do
-      it "becomes pending if failed" do
-        todo("will raise hoge") { raise "hoge" }
+      it_behaves_like "pending", "when expectation failed" do
+        let(:expectation) do
+          todo("with raise hoge") { raise "hoge" }
+        end
       end
     end
 
@@ -13,8 +62,62 @@ describe "rspec-todo" do
         todo { true }
       end
 
-      it "becomes pending if failed" do
-        todo { raise "hoge" }
+      it_behaves_like "pending", "when expectation failed" do
+        let(:expectation) do
+          todo { raise "hoge" }
+        end
+      end
+    end
+
+    context "with if opts" do
+      context "are true" do
+        it "passes" do
+          todo(if: true) { true }
+        end
+
+        it_behaves_like "pending", "when expectation failed" do
+          let(:expectation) do
+            todo(if: true) { raise "hoge" }
+          end
+        end
+      end
+
+      context "are false" do
+        it "passes" do
+          todo(if: false) { true }
+        end
+
+        it_behaves_like "error", "when expectation failed" do
+          let(:expectation) do
+            todo(if: false) { raise "hoge" }
+          end
+        end
+      end
+    end
+
+    context "with unless opts" do
+      context "are false" do
+        it "passes" do
+          todo(unless: false) { true }
+        end
+
+        it_behaves_like "pending", "when expectation failed" do
+          let(:expectation) do
+            todo(unless: false) { raise "hoge" }
+          end
+        end
+      end
+
+      context "are true" do
+        it "passes" do
+          todo(unless: true) { true }
+        end
+
+        it_behaves_like "error", "when expectation failed" do
+          let(:expectation) do
+            todo(unless: true) { raise "hoge" }
+          end
+        end
       end
     end
   end
@@ -31,7 +134,13 @@ describe "rspec-todo" do
     end
 
     it "results nothing when specified error occured" do
-      not_todo("runtime error occured", RuntimeError) do
+      not_todo("runtime error occured", errors: RuntimeError) do
+        raise
+      end
+    end
+
+    it "results nothing when specified errors occured" do
+      not_todo("runtime error occured", errors: [ RuntimeError ]) do
         raise
       end
     end
